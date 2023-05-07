e5678b42fb5425acaf2b914d8bc11df8722202e1
chensmm <chensmm@163.com>
2023-05-07 15:47:12 +0800

    井字棋基础玩法实现

--- Assets/Script/Chessboard.cs
+++ Assets/Script/Chessboard.cs
@@ -1,61 +1,132 @@
 ﻿using System.Collections;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Assets.Script
 {
-    public class Chessboard
+    public class Chessboard : MonoBehaviour
     {
         public int boardSize { get; private set; }
         public int[,] chessboard { get; private set; }
 
-        public int turn = 1;//player1=1;player2=2;空白=0
+        private int turnNum = 0;
+        private bool player1Trun = true;
+        public GameObject gridPrefab;
 
-
-        public Chessboard(int size)
+        private void Start()
         {
-            boardSize = size;
-            ChessboardInit(boardSize);
+            Init(3);
         }
 
-        public void ChessboardInit(int boardSize)
+        public void Init(int size)
         {
+            boardSize = size;
             chessboard = new int[boardSize, boardSize];
             for (int i = 0; i < boardSize; i++)
             {
                 for (int j = 0; j < boardSize; j++)
                 {
-                    chessboard[i, j] = -1;
+                    chessboard[i, j] = 0;
                 }
+            }
+            CreatChessboard();
+        }
+
+        public void CreatChessboard()
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    GameObject grid = GameObject.Instantiate(gridPrefab, this.gameObject.transform);
+                    grid.GetComponent<Grid>().OnInit(i, j);
+                }
+
             }
         }
 
-        public void SetChess(int x, int y, int player)
+        public int SetChess(int x, int y)
         {
-            chessboard[x, y] = player;
-
+            if (player1Trun)
+            {
+                chessboard[x, y] = 1;
+            }
+            else
+            {
+                chessboard[x, y] = -1;
+            }
+            turnNum++;
+            player1Trun = !player1Trun;
+            int isWin = IsWin(x, y);
+            if (isWin == 0)
+            {
+                //检测是否平局
+                if (turnNum == boardSize * boardSize)
+                {
+                    Debug.Log("Draw!");//平局结算
+                }
+            }
+            else
+            {
+                Debug.Log("Player" + isWin + " Win!!!");//获胜结算
+            }
+            return chessboard[x, y];
         }
-        private int IsWin(int x, int y, int player)
+        private int IsWin(int x, int y)
         {
             bool isWin = true;
-            //检测横排
+            //检测竖排
+            int value = 0;
             for (int i = 0; i < boardSize; i++)
             {
-                if (chessboard[x, i] != player)
+                value += chessboard[x, i];
+            }
+            if (Mathf.Abs(value) == boardSize)//player
+            {
+                return value / boardSize;
+            }
+            //检测横排
+            value = 0;
+            for (int i = 0; i < boardSize; i++)
+            {
+                value += chessboard[i, y];
+            }
+            if (Mathf.Abs(value) == boardSize)
+            {
+                return value / boardSize;
+            }
+            //检测45斜方向
+            value = 0;
+            if (x == y)
+            {
+                for (int i = 0; i < boardSize; i++)
                 {
-                    isWin = false;
+                    value += chessboard[i, i];
                 }
             }
-            //检测竖排
-            for (int i = 0; i < boardSize; i++)
+            if (Mathf.Abs(value) == boardSize)
             {
-                if (chessboard[i, y] != player)
+                return value / boardSize;
+            }
+            //检测135斜方向
+            value = 0;
+            if (x + y == boardSize)
+            {
+                for (int i = 0; i < boardSize; i++)
                 {
-                    isWin = false;
+                    value += chessboard[boardSize - i - 1, i];
                 }
+            }
+            if (Mathf.Abs(value) == boardSize)
+            {
+                return value / boardSize;
             }
 
             return 0;
         }
+
+
 
     }
 }
---
+++ Assets/Script/Grid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Script
+{
+    public class Grid : MonoBehaviour
+    {
+        TMP_Text text;
+        Chessboard chessboard;
+        int x;
+        int y;
+        private int belongTo = 0;//1=player1,-1=player2
+        public void OnInit(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            text = this.gameObject.GetComponentInChildren<TMP_Text>();
+            text.text = " ";
+            chessboard = GetComponentInParent<Chessboard>();
+        }
+
+        public void OnClick()
+        {
+            int turn = chessboard.SetChess(x, y);
+            if (turn == 1)
+            {
+                text.text = "O";
+            }
+            else
+            {
+                text.text = "X";
+            }
+            Debug.Log("Press:" + x + "," + y);
+        }
+
+    }
+}
